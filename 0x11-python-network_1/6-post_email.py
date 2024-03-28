3d133e1ff41374836474179ae74edb2fc16bbd9f
Deborah3321 <nchabelengdeborah18@gmail.com>
2024-03-28 03:39:52 -0700

    python network 1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-POST request to the passed URL with the email as a parameter
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import requests
 from sys import argv
 
 
-def main(argv):
-    """
-    Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-    """
-    values = {'email': argv[2]}
-    url = argv[1]
-    r = requests.post(url, data=values)
-    print(r.text)
-
 if __name__ == "__main__":
-    main(argv)
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
